eb4e806d9475fa98cd55318797d80c40dd689416
anzhijiangll <1336979712@qq.com>
2019-05-04 14:59:36 +0800

    jll

--- html/js/fontSize.js
+++ html/js/fontSize.js
@@ -7,15 +7,56 @@
     var recalCulate = function(){
         var width = docEle.clientWidth;
         var height = docEle.clientHeight;
-        if (width > 1024) {//最大宽度640px
-            width =1024;
+        if (width > 750) {//最大宽度640px
+            width =750;
         }
         docEle.style.fontSize = 75 * (width / 750) + 'px';//以宽为640px为基准，rem为20px
     };
     recalCulate();
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvent, recalCulate, false);
-    var icon_css ='<link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1166350_bj01j5cgrzu.css"/>';
+    //加载自体图标
+    var icon_css ='<link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1166350_w6uf8sme3v9.css"/>';
     var head = $("head");
     head.append(icon_css);
+    /*搜索*/
+	/*$(".search").click(function(){
+		$(".search_icon").css("display","none");
+		$(".search_input").css("display","block");
+		$(".search_input").focus();
+	});
+	$(".search_input").blur(function(){
+		$(this).css("display","none");
+		$(".search_icon").css("display","flex");
+	});*/
+    //菜单
+    $("#nav").click(function(){
+    	$("#menu").addClass("menu_animation");
+    });
+    $("#menu").click(function(){
+	    $("#menu").removeClass("menu_animation");
+	});
+	$(".menu_con").click(function(event){
+	    event.stopPropagation();
+	});
+	//购物车
+	$("body,html").click(function(){
+		$(".shopping_con").css("display","none");
+	});
+	$("#shopping").click(function(event){
+		event.stopPropagation();
+		var block = $(this).children(".shopping_con").css("display");
+		if(block=='none'){
+			$(this).children(".shopping_con").css("display","block");
+		}else{
+			$(this).children(".shopping_con").css("display","none");
+		}
+	});
+	$(".shopping_con").click(function(event){
+		event.stopPropagation();
+	});
+	//底部导航颜色切换
+	$(".center_footer_nav li").click(function(){
+		$(this).children('a').addClass('color_blue').siblings().removeClass('color_blue');
+	});
 })(document, window);
