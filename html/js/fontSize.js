b094460bcd8f2587f8534e826113a1790370b653
蒋燎燎 <1336979712@qq.com>
2019-05-20 10:21:12 +0800

    jll

--- html/js/fontSize.js
+++ html/js/fontSize.js
@@ -1,26 +1,26 @@
 ;(function (doc, win) {
 	//js页面自适应
-//  var docEle = doc.documentElement;
-//
-//  var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
-// 
-//  var recalCulate = function(){
-//      var width = docEle.clientWidth;
-//      var height = docEle.clientHeight;
-//      if (width > 1024) {//最大宽度640px
-//          width =1024;
-//      }
-//      docEle.style.fontSize = 75 * (width / 750) + 'px';//以宽为640px为基准，rem为20px
-//  };
-//  recalCulate();
-//  if (!doc.addEventListener) return;
-//  win.addEventListener(resizeEvent, recalCulate, false);
+ var docEle = doc.documentElement;
+
+ var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
+
+ var recalCulate = function(){
+     var width = docEle.clientWidth;
+     var height = docEle.clientHeight;
+     if (width > 1024) {//最大宽度640px
+         width =1024;
+     }
+     docEle.style.fontSize = 75 * (width / 750) + 'px';//以宽为640px为基准，rem为20px
+ };
+ recalCulate();
+ if (!doc.addEventListener) return;
+ win.addEventListener(resizeEvent, recalCulate, false);
     //加载自体图标
     var icon_css ='<link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1166350_j99lf7bi6ll.css"/>';
-    var flexibleJs ='<script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.2/??flexible_css.js,flexible.js"></script>';
+    // var flexibleJs ='<script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.2/??flexible_css.js,flexible.js"></script>';
     var head = $("head");
     head.append(icon_css);
-    head.append(flexibleJs);
+    // head.append(flexibleJs);
     /*搜索*/
 	/*$(".search").click(function(){
 		$(".search_icon").css("display","none");
