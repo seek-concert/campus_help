a2d30fd3edc271c17d33a9e7a7935bc3a309288b
jll <1336979712@qq.com>
2019-05-31 04:01:21 +0800

    驾驶员端

--- html/js/fontSize.js
+++ html/js/fontSize.js
@@ -16,7 +16,7 @@
  if (!doc.addEventListener) return;
  win.addEventListener(resizeEvent, recalCulate, false);
     //加载自体图标
-    var icon_css ='<link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1166350_134wj8hgxgd.css"/>';
+    var icon_css ='<link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1166350_wwi75vw7uwa.css"/>';
     // var flexibleJs ='<script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.2/??flexible_css.js,flexible.js"></script>';
     var head = $("head");
     head.append(icon_css);
